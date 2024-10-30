1726bb5cf41adfb845d45adb4d82e882daf7e1b6
Hardcopy Music Programming <carlmcdade@gmail.com>
2024-10-30 10:05:24 +0100

    Create install.php

---
+++ install/install.php
@@ -0,0 +1,28 @@
+
+
+
+<?php
+$path_parts = pathinfo($_SERVER['DOCUMENT_ROOT']."/cck/databases");
+$path_partr = pathinfo(__FILE__);
+
+?>
+
+<?php 
+$db_location = $_SERVER['DOCUMENT_ROOT']."/cck/databases/".'administration.db';
+
+if(file_exists($db_location)==TRUE)
+{ echo "file already found";
+}else{
+	$db = new SQLite3($db_location);
+$db->exec('CREATE TABLE content_types (bar TEXT)');
+$db->exec("INSERT INTO content_types (bar) VALUES ('this is a content type')");
+$result = $db->query('SELECT * FROM content_types');
+var_dump($result->fetchArray());
+};
+
+
+
+var_dump($path_parts);
+var_dump($path_partr);
+
+?>
