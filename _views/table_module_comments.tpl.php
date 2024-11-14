8142792b9081520e752048a23f4b6ed0d308ed41
Hardcopy Music Programming <carlmcdade@gmail.com>
2024-11-14 01:53:59 +0100

    inline documentation stuff

--- _views/table_module_comments.tpl.php
+++ _views/table_module_comments.tpl.php
@@ -1,12 +1,35 @@
-<?php 
-$output = '<table class="table table-striped">';
-$output .= '<thead><tr>';
-// table header
+<?php
+//var_dump($moduleMethods);
+$output = "";
+$method_output = '';
+$comment_titles = '';
+/*  methods list */
+ $method_output ='<div id="list-example" class="list-group">';
+ $comment_titles = '<div data-spy="scroll" data-target="#list-example" data-offset="0" class="scrollspy-example">' ."\n";
+
+    foreach($moduleMethods as $key => $name){
+    
+      $method_output .= '<a class="list-group-item list-group-item-action fs-6" href="#list-item-'.$key.'">'.$name."</a><br>\n";
+      
+      $comment_titles .= '<h5 class="m-0" style="padding:.25em 0 .25em 0; border-bottom: 2px solid #777;background-color: #ffffff;" id="list-item-'.$key.'">'.$name.'</h5>';
+      $comment_titles .= '<pre class="m-0" style="max-width:100%;overflow-x:auto; background-color: #fff999;">'. ($moduleComments[$key] != FALSE ? $moduleComments[$key]: 'no developer comments').'</pre>'."\n";
+    }
+$method_output .= '</div>';
+$comment_titles .= '</div>';
+
+
+$output .= '</div>' . "\n";
+$output .= '<div class="container"><div style="border-style:solid;border-width:1px 0 1px 0;border-color:#cccccc;" class="row">';
 foreach ($header as $th => $column)
 {
-	$output .= '<th scope ="col">' .  $column . '</th>' . "\n";	
+	$span = ($th == 0 ? 8 : 4);
+	$output .= '<div class="fs-4 col-'.$span.'">' .  $column . '</div>' . "\n";	
 }
-$output .= '</tr></thead>' . "\n";
+$output.= '</div>';
+$output .= '<div style="height:640px; overflow-y:scroll;" class="container">';
+$output .= '<div class="row"></div>';
+// table header
+
 //var_dump($rows);
 if(!empty($rows))
 {
@@ -17,7 +40,7 @@ if(!empty($rows))
 
 	foreach ($rows as $tr => $row)
 	{
-		$output .= '<tr id="" class="table-cells">';	
+		$output .= '<div class="row">';	
 	foreach($row as $key=> $value){
 		// table cells per row
 			if($key == 1)
@@ -27,16 +50,18 @@ if(!empty($rows))
 			}
 			elseif($key == 0)
 			{
-				$output .= '<td id="td-' . (isset($id) ? $id : '')  . $tr . '-' .  $tr . '" class="table-cells"><a href="?admin/module_edit/'.$row[0].'">configure</a></td>'.
-				'<td>'. $row[1]. '</td><td><a href="?admin/module_comments_view/' . $row[0].'">'.'administer'.'</a></td>'. 
+				//$output .= '<td id="td-' . (isset($id) ? $id : '')  . $tr . '-' .  $tr . '" class="table-cells"><a href="?admin/module_edit/'.$row[0].'">configure</a></td>';
+				$output .= '<div style="overflow-y:scroll" class="col-8">'. $row[1]. $comment_titles . '</div>'."\n".'<div class="col-4" style=";text-align: center;color: #ffffff;background-color:#666"><br>'. 
 				'' . "\n";
 			}
-		}
-	$output .= '</tr>' . "\n";	
+			
+	}	
+	$output .= "". $method_output. "</div>\n";
+	$output .= '</div>' . "\n";	
 	}
     
 }
-$output .= '</table>';
+$output .= '</div>';
 
 print $output;
 //var_dump($row);
